04551cf05b15f00705f5f1583bf2fc9b4298525f
KevDog1996 <75052714+KevDog1996@users.noreply.github.com>
2021-09-24 21:22:32 -0500

    Create Agents Table
    .

---
+++ SQLQuery2.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Agents
+(AgentID varchar(50),
+AgentFirstName varchar(50),
+AgentLastName varchar(50),
+AgentAddress varchar(50),
+AgentCity varchar(50),
+AgentState varchar(2),
+AgentZip int,
+Phone varchar(10),
+DateHired date,
+Salary money,
+CommissionRate decimal
+)
+
